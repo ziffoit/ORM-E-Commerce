6d822157efb4101b5f01bafde847fb90d71a4e74
Kevin Hanson <ziffoit@gmail.com>
2021-05-10 10:26:01 -0600

    initial setup and category.js

---
+++ .history/Develop/models/Category_20210510102019.js
@@ -0,0 +1,23 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
---
+++ .history/Develop/models/Category_20210510102033.js
@@ -0,0 +1,24 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
